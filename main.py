2878dd01b5ed64553e3c1f8629b7f8291c1f30d3
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2023-05-27 12:30:47 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,190 @@
+import openai
+import pyaudio
+import wave
+import json
+import re
+import time
+import threading
+
+import keyboard
+
+# Utils
+from utils import get_prompt, speech_text
+
+
+# Setup
+from config import OPENAI_API_KEY, OPENAI_ORGANIZATION
+# openai.organization = OPENAI_ORGANIZATION
+openai.api_key = OPENAI_API_KEY
+
+# setup var
+user_name = "System"
+
+# app var
+current_user_message = ""
+prev_user_message = ""
+conversation = []
+history = {"history" : conversation}
+auto_speech_count = 10
+is_speaking = False
+
+
+def handle_from_input(text):
+    global is_speaking
+    is_speaking = True
+
+    print("Questions: " + text)
+    result = user_name + " berkata " + text 
+    add_conversation("user", result)
+
+    duration = speech_text(f"{user_name} bilang {text}")
+
+    # time.sleep(duration)
+    chat_gpt_generate()
+
+
+def record_audio():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=CHUNK,
+    )
+
+    print('recording...')
+
+    frames = []
+
+    while keyboard.is_pressed('space'): 
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+
+    print('stoped')
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open("recording.wav", "wb")
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    format_audio_to_text("recording.wav")
+
+def format_audio_to_text(file):
+    global is_speaking
+    try:
+        file_audio = open(file, "rb")
+
+        transcript = openai.Audio.transcribe("whisper-1", file_audio)
+        message = transcript.text
+        print(f"Questions : {message}")
+        
+        result = user_name + " berkata " + message
+        add_conversation("user", result)
+        is_speaking = True
+        chat_gpt_generate()
+    except Exception as e:
+        print("Error in formating audio: {0}".format(e))
+        return
+
+def chat_gpt_generate():
+    global auto_speech_count, is_speaking
+
+    total_characters = sum(len(d['content']) for d in conversation)
+    while total_characters > 4000:
+        try:
+            conversation.pop(2) # 2 because the first one is the questions
+            total_characters = sum(len(d['content']) for d in conversation)
+        except Exception as e:
+            print("Error removing old messages: {0}".format(e))
+
+    with open("conversation.json", "w", encoding="utf-8") as f:
+        json.dump(history, f, indent=4)
+
+    prompt_data = get_prompt()
+
+    try:
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = prompt_data,
+            max_tokens = 128,
+            temperature = 1,
+            top_p = 0.9,
+            frequency_penalty = 2.0,
+            presence_penalty = 2.0,
+        )
+        message = response['choices'][0]['message']['content']
+        clean_message = re.sub(r'[:;)(@]', '', message)
+        conversation.append({'role': 'assistant', 'content': clean_message})
+        print(f"Assistant: {clean_message}")
+        speech_text(clean_message)
+
+        is_speaking = False
+        auto_speech_count = 10
+    except Exception as e:
+        print("Generate response Error: {0}".format(e))
+    
+
+
+
+def add_conversation(role, message):
+    global conversation, current_user_message
+    current_user_message = message
+    conversation.append({"role": role, "content": current_user_message})
+
+
+def auto_speech():
+    global auto_speech_count
+
+    while True:
+        time.sleep(1)
+        auto_speech_count -= 1
+        print(auto_speech_count)
+
+        # if auto_speech_count <= 0 and not is_speaking:
+        #     add_conversation("system", "halo")
+        #     chat_gpt_generate()
+
+
+
+if __name__ == "__main__":
+
+    user = input("Accessing as: ")
+    user_name = user
+    mode = input("Mode: ")
+    try:
+        if mode == "1":
+            print('hold SPACE to record and wait until "recording..." displayed before speaking')
+            t = threading.Thread(target=auto_speech)
+            t.start()
+            while True:
+                if keyboard.is_pressed('space'):    
+                    record_audio()
+
+        elif mode == "2":
+            t = threading.Thread(target=auto_speech)
+            t.start()
+            while True:
+                text = input('say something: ')
+                handle_from_input(text=text)
+        
+        else:
+            print('Mode not found')
+
+    except KeyboardInterrupt:
+        t.join()
+        print("Thread Stopped")
+             
+
---
+++ utils.py
@@ -0,0 +1,113 @@
+import json
+import os
+import tempfile
+
+import subprocess
+import audiosegment
+from pydub import AudioSegment
+from IPython.display import Audio, display
+from playsound import playsound
+from mutagen.mp3 import MP3
+
+AudioSegment.ffmpeg = r'C:\ffmpeg\bin'
+
+
+def get_identity(identity_path):  
+    with open(identity_path, "r", encoding="utf-8") as f:
+        identity_context = f.read()
+        identity_context = identity_context.replace('\n', ' ')
+    return {"role": "system", "content": identity_context}
+
+
+def get_prompt():
+    prompt = []
+    identity = get_identity("character/eve.txt")
+    prompt.append(identity)
+    
+    with open("conversation.json", "r") as f:
+        data = json.load(f)
+        history = data['history']
+        
+    if len(history) > 1:
+        prompt.append({
+            "role": "system",
+            "content": f"Below is conversation history.\n"
+        })
+        for message in history[:-1]:
+            prompt.append(message)
+        
+    prompt.append({
+        "role" : "system",
+        "content": "Here is the latest conversation.\n*Make sure your response is within 100 characters!\n"
+    })
+
+    if len(history) != 0:
+        prompt.append(history[-1])
+
+    return prompt
+
+
+def speech_text(text_input):
+    gender = "Female" #@param ["Male", "Female"]
+    text = text_input
+
+    if gender == "Male":
+        command = ['edge-tts', '--voice', 'id-ID-ArdiNeural', '--text', text, '--write-media', 'edge.mp3', '--write-subtitles', 'edge.vtt']
+        result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+        print(result.stdout)
+    elif gender == "Female":
+        command = ['edge-tts', '--voice', 'id-ID-GadisNeural', '--text', text, '--write-media', 'edge.mp3', '--write-subtitles', 'edge.vtt']
+        result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+        print(result.stdout)
+
+    try:
+        audio = Audio("edge.mp3")
+    except Exception as e:  print("Error:", str(e))
+
+    audio_mp3 = MP3('edge.mp3')
+    duration = audio_mp3.info.length
+
+    try:
+        if not os.access("edge.mp3", os.R_OK):
+            print("Cannot access the file:", "edge.mp3")
+            return
+
+        temp_filename = os.path.join(tempfile.gettempdir(), 'temp.mp3')
+
+        with open("edge.mp3", 'rb') as file:
+            with open(temp_filename, 'wb') as temp_file:
+                temp_file.write(file.read())
+            
+        playsound(temp_filename)
+        os.remove(temp_filename)
+    except Exception as e:
+        print("Error:", str(e))
+
+    return duration
+    
+    # audio = audiosegment.from_file("edge.mp3")
+    # # Set the output format to WAV
+    # audio = audio.set_sample_width(2)
+    # audio = audio.set_frame_rate(44100)
+    # audio = audio.set_channels(1)
+    # # Export the audio to WAV format
+    # audio.export("edge-conv.wav", format='wav')
+
+    # AUDIO = "edge-conv" #@param {type:"string"}
+    # MODEL = "/content/so-vits-test/alice.pth" #@param {type:"string"}
+    # CONFIG = "/content/so-vits-test/config.json" #@param {type:"string"}
+    # METHOD = "harvest" #@param ["harvest", "dio", "crepe", "crepe-tiny", "parselmouth"]
+    # PITCH = 0 #@param {type:"slider", min:-12, max:12, step:1}
+
+    # # Auto Pitch Mode
+    # command = f"svc infer {AUDIO}.wav -c {CONFIG} -m {MODEL} -fm {METHOD}"
+    # subprocess.run(command, shell=True)
+
+    # # Manual Pitch Mode
+    # # !svc infer {AUDIO}.wav -c {CONFIG} -m {MODEL} -fm {METHOD} -na -t {PITCH}
+
+    # try:
+    #     display(Audio(f"{AUDIO}-out.wav", autoplay=True))
+    # except Exception as e:  print("Error:", str(e))
+
+            
